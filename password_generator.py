fcbf72e9e5dd7627deb2d5ac883ad23113fe1e5f
ProfaniDee <devon.lohrmann@gmail.com>
2022-12-15 22:52:41 -0500

    password generator

---
+++ password_generator.py
@@ -0,0 +1,17 @@
+import random
+import string
+
+print('Hello, Welcome to the password generator.\n')
+length = int(input('How many characters in length would you like your password to be?\n'))
+
+lower = string.ascii_lowercase
+upper = string.ascii_uppercase
+num = string.digits
+symbols = string.punctuation
+
+all = lower + upper + num + symbols
+
+temp = random.sample(all,length)
+
+password = ''.join(temp)
+print('Your custom password is :   ' + password)
